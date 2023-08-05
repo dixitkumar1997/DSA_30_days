4f256dcc62fce4ee0de2101a5fc32ffc44328348
sarcasmic guy <dxtt22@gmail.com>
2023-08-05 23:14:26 +0530

    third day array problems

---
+++ Array/Problem-8/Problem-8.js
@@ -0,0 +1,21 @@
+// Ques:- You are given a 1-indexed integer array nums of length n.
+
+// An element nums[i] of nums is called special if i divides n, i.e. n % i == 0.
+
+// Return the sum of the squares of all special elements of nums.
+
+// Soln:-
+
+
+let  nums = [1,2,3,4];
+ function sumOfSquares(nums) {
+    let n=nums.length;
+    let sum=0;
+    for(let i=0;i<n;i++){
+        if(n%(i+1)==0){
+sum+=(nums[i]*nums[i])
+        }
+    }
+    return sum;
+};
+console.log(sumOfSquares(nums));
