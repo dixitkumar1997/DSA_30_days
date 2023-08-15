89b7575fbd1b52ce9ce67d1f79ff67d2f05262f1
sarcasmic guy <dxtt22@gmail.com>
2023-08-16 00:15:08 +0530

    fifteenth day array problems

---
+++ Array/Problem-15/Problem-15.js
@@ -0,0 +1,29 @@
+// Ques:-You are given an integer array nums. The unique elements of an array are the elements that appear exactly once in the array.
+
+// Return the sum of all the unique elements of nums.
+
+//  Soln:-
+
+
+let nums = [1,2,3,2];
+
+function sumOfUnique(nums) {
+    let obj={};
+    let sum=0;
+    for(let i=0;i<nums.length;i++){
+        if(!obj[nums[i]]){
+            obj[nums[i]]=1;
+        }
+        else{
+            obj[nums[i]]+=1;
+        }
+    }
+    for(let key in obj){
+        if(obj[key]===1){
+            sum+=+key
+        }
+    }
+    return sum
+};
+
+console.log(sumOfUnique(nums))
