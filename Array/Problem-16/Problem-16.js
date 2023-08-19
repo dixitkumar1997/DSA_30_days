a4cf6c08c59bbc06f80c606e1fb91a3748dc9f17
sarcasmic guy <dxtt22@gmail.com>
2023-08-19 22:59:57 +0530

    eighteenth day array problems

---
+++ Array/Problem-16/Problem-16.js
@@ -0,0 +1,19 @@
+// Given an array nums of integers, return how many of them contain an even number of digits.
+
+//  Soln:-
+
+
+let nums = [12,345,2,6,7896];
+
+function findNumbers(nums) {
+    let count=0;
+    for(let i=0;i<nums.length;i++){
+        let str=nums[i]+"";
+        if(str.length%2===0){
+            count+=1
+        }
+    }
+    return count;
+};
+
+console.log(findNumbers(nums))
